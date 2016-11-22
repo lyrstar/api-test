a97f5a002497c838a1a3de58965cc558501cfc46
sunpengfei <rrlyrstar@163.com>
2016-11-22 14:15:56 +0800

    log

--- agent.js
+++ agent.js
@@ -20,10 +20,8 @@ function send(req, res, next){
     let path = req.url;
     let method = req.method;
     let data = req.query;
-    console.log(req.url);
-    console.log(req.method);
-    console.log(req.body);
-    console.log(req.query);
+    console.log(req.method, req.url);
+    console.log(req.params, req.query, req.body);
     if(req.url === '/favicon.ico'){
         res.send('');
         return;
